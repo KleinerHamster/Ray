0db4e15d21c080817ad68d7398b6affd6aa70db9
Kleiner Hamster <viktoria.ivashova.04@mail.ru>
2023-01-21 11:29:08 +0300

    Create triangle

--- Ray/ViewController.swift
+++ Ray/ViewController.swift
@@ -9,49 +9,30 @@ import Cocoa
 import MetalKit
 
 enum Colors{
-    static let wenderlichGreen = MTLClearColor(red: 0.25,
+    static let wenderlichGreen = MTLClearColor(red: 0.4,
                                                green: 0.76,
                                                blue: 0.8,
                                                alpha: 1.0)
 }
-
+ 
 class ViewController: NSViewController {
 
     var metalView: MTKView{
         return view as! MTKView
     }
     
-    var device: MTLDevice!
-    var commandQueue: MTLCommandQueue!
+    var renderer: Renderer?
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        metalView.device = MTLCreateSystemDefaultDevice()
-        device = metalView.device
-        
-        metalView.clearColor = Colors.wenderlichGreen
-        metalView.delegate = self
-        commandQueue = device.makeCommandQueue()
-        
-    }
-}
-
-extension ViewController: MTKViewDelegate{
-    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize){
-        
-    }
-    func draw(in view: MTKView) {
-        guard let drawble = view.currentDrawable,
-              let descriptor = view.currentRenderPassDescriptor else{
-            return }
-        
-        let commandBuffer = commandQueue.makeCommandBuffer()
-        
-        let commandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: descriptor)
-        
-        commandEncoder?.endEncoding()
-        commandBuffer?.present(drawble)
-        commandBuffer?.commit()
+      super.viewDidLoad()
+      metalView.device = MTLCreateSystemDefaultDevice()
+      guard let device = metalView.device else {
+        fatalError("Device not created. Run on a physical device")
+      }
+      
+      metalView.clearColor =  Colors.wenderlichGreen
+      renderer = Renderer(device: device)
+      metalView.delegate = renderer
     }
 }
 
